f4b9512caaedcb51ef079d0a54d3ede1ba515f8a
elfaroliver <eos35@hi.is>
2025-01-29 22:15:58 +0000

    Fleiri cells í Tímadæmi2.1

--- Vika3/Tímadæmi2.py
+++ Vika3/Tímadæmi2.py
@@ -1,6 +1,8 @@
 #class Tímadæmi2:
+import math
+
 def annadStig ():
-    import math
+    #import math
 
     #a,b,c = float(input("Sláðu inn a, b, og c fyrir annars stigs jöfnu: "))
     a = float(input("Sláðu inn gildi fyrir a: "))
@@ -18,3 +20,19 @@ def annadStig ():
         x1 = (-b+math.sqrt(b**2-4*a*c))/(2*a)
         x2 = (-b-math.sqrt(b**2-4*a*c))/2*a
         print("Tvær lausnir"), x1, " og", x2
+
+def Trapisa (f):
+    #import math
+
+    a = float(input("Sláðu inn gildi fyrir a: "))
+    b = float(input("Sláðu inn gildi fyrir b: "))
+    n = int(input("Sláðu inn gildi fyrir n: "))
+
+    h = (b - a) / n
+    sum = f(a) + f(b)
+
+    for i in range(1, n):
+        x_i = a + i * h
+        sum += 2 * f(x_i)
+
+    return sum * (h / 2)
