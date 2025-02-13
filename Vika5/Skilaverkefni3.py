acc4f860ab994d66963f74af781a11524ac6f961
elfaroliver <eos35@hi.is>
2025-02-13 14:23:41 +0000

    arr_to_dict örugglega komið í Skilaverkefni3. Jup file líka byrjað

--- Vika5/Skilaverkefni3.py
+++ Vika5/Skilaverkefni3.py
@@ -1,14 +1,16 @@
 class Skil(object):
     """Skilaverkefni 3 f. REI"""
     def arr_to_dict(self, arr):
-        '''Skrifið fall arr_to_dict(arr) þar sem tekur inn fylki og skilar uppflettitöflu. Lyklarnir í töflunni eru staðsetningar í fylkinu og eru tjáðir með tvenndinni (röð, dálkur). Gildin í töflunni eru gildin í fylkinu með tilsvarandi staðsetningu. Ef d = arr_to_dict(arr) þá gildir að d[(i, j)] == arr[i][j].
+        '''Skrifið fall arr_to_dict(arr) þar sem tekur inn fylki og skilar uppflettitöflu. 
+        Lyklarnir í töflunni eru staðsetningar í fylkinu og eru tjáðir með tvenndinni (röð, dálkur). 
+        Gildin í töflunni eru gildin í fylkinu með tilsvarandi staðsetningu. Ef d = arr_to_dict(arr) þá gildir að d[(i, j)] == arr[i][j].
 
         Inntak: arr er ferningsfylki sem tjáð er sem listi af listum (sjá að ofan).
         Skilar: Uppflettitöflu d sem geymir sérhvert stak arr[i][j] með lykli (i, j).
         '''
 
 
-        
+        return {(i, j): arr[i][j] for i in range(len(arr)) for j in range(len(arr[i]))}
         ...
     def dict_to_arr(self, d):
 
