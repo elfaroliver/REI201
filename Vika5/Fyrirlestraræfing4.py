879dc29ce025bf7f6082dd28c1d1000eed626570
elfaroliver <eos35@hi.is>
2025-02-13 11:44:46 +0000

    Fyrirlestraræfing4 10/10 á Gradescope

---
+++ Vika5/Fyrirlestraræfing4.py
@@ -0,0 +1,21 @@
+
+
+import numpy
+
+class Skil(object):
+    """Fyrirlesraraefing 4 f. REI
+
+    """
+    def numpy_test(self,ls):
+        ...
+        #arr = input(ls)
+
+        arr = numpy.array(ls)
+        arr_t = numpy.transpose(arr)
+
+        return arr,arr_t
+
+
+#if __name__ == '__main__':
+    #skil = Skil()
+
--- Vika5/Skilaverkefni3.py
+++ Vika5/Skilaverkefni3.py
@@ -1,8 +1,14 @@
 class Skil(object):
     """Skilaverkefni 3 f. REI"""
     def arr_to_dict(self, arr):
+        '''Skrifið fall arr_to_dict(arr) þar sem tekur inn fylki og skilar uppflettitöflu. Lyklarnir í töflunni eru staðsetningar í fylkinu og eru tjáðir með tvenndinni (röð, dálkur). Gildin í töflunni eru gildin í fylkinu með tilsvarandi staðsetningu. Ef d = arr_to_dict(arr) þá gildir að d[(i, j)] == arr[i][j].
+
+        Inntak: arr er ferningsfylki sem tjáð er sem listi af listum (sjá að ofan).
+        Skilar: Uppflettitöflu d sem geymir sérhvert stak arr[i][j] með lykli (i, j).
+        '''
 
 
+        
         ...
     def dict_to_arr(self, d):
 
